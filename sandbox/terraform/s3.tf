761cf6b98ec29979b059281b24d2b3606bcb7051
awsragook2 <awsragook2@gmail.com>
2025-11-19 15:39:29 -0500

    added customization

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -6,7 +6,3 @@ resource "aws_s3_bucket" "sandbox_bucket" {
 }
 
 
-resource "aws_s3_bucket" "sandbox_bucket22" {
-  bucket = "aft-sandbox22-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
-}
